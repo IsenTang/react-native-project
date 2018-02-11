74e26c92ca66ab916b1858a56a16d13b6408d57d
isenTang <3104255643@qq.com>
2018-02-11 10:24:57 -0800

    init

---
+++ src/containers/index.js
@@ -0,0 +1,17 @@
+import {NativeRouter, Route, Link} from 'react-router-native'
+import {View,Text} from 'react-native';
+
+
+
+
+export default class Index extends Component {
+    render() {
+        return (
+            <NativeRouter>
+                <View>
+                    <Route path='/' component={}/>
+                </View>
+            </NativeRouter>
+        )
+    }
+}
