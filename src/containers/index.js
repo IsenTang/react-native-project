6fa60391c25d0605671953f312df716f2572294d
isenTang <3104255643@qq.com>
2018-02-14 12:32:55 -0800

    init

--- src/containers/index.js
+++ src/containers/index.js
@@ -17,6 +17,11 @@ export default StackNavigator(
   },
   {
     initialRouteName: 'Home',
+    // mode: 'modal',
+    headerMode: 'none',
+    // transitionConfig：配置页面跳转的动画
+    // onTransitionStart: 页面跳转动画即将开始的回调
+    // onTransitionEnd: 页面跳转动画结束的回调
   },
 );
 
