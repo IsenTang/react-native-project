725e2e6e112d1bca0c8ecd1da6a16fc21ed65339
isenTang <3104255643@qq.com>
2018-02-13 16:29:04 -0800

    init

--- src/services/demo.js
+++ src/services/demo.js
@@ -1,9 +1,21 @@
-import request from '../util/request'
+import request from '../util/request';
 
 export async function getMenu(data) {
-    return await request({
-        url: '/system/getMenu',
-        method: 'post',
-        data
-    })
+  const result = await request({
+    url: '/system/getMenu',
+    method: 'post',
+    data,
+  });
+
+  return result;
+}
+
+export async function test(data) {
+  const result = await request({
+    url: '/system/getMenu',
+    method: 'post',
+    data,
+  });
+
+  return result;
 }
--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,5 @@
 import demo from './demo';
+
 module.exports = {
-    demo
+  demo,
 };
